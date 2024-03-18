1b41e7b10af96f1464e926a40f39d9489c05b957
Jason.z <281674669@qq.com>
2024-03-18 21:17:18 +0800

    修改安装脚本

---
+++ build-iso.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+cd /mnt/
+
+sudo apt-get update && sudo apt-get install -y curl xorriso squashfs-tools syslinux syslinux-efi isolinux fakeroot dumpet gddrescue
+
+curl -X GET -OL https://releases.ubuntu.com/20.04.4/ubuntu-20.04.4-live-server-amd64.iso
+
+xorriso -osirrox on -indev "ubuntu-20.04.4-live-server-amd64.iso" -extract / iso && chmod -R +w iso
+
+
+cp iso/casper/filesystem.squashfs .
+
+sudo unsquashfs filesystem.squashfs
+
+sudo chroot squashfs-root/ 
+
+mount none -t proc /proc; mount none -t sysfs /sys; mount none -t devpts /dev/pts
+
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+
+sudo apt-get update && sudo apt-get install -y wget git zip
+
+wget https://github.com/byteee-com/bpbx-scripts/archive/refs/heads/main.zip
+
+unzip main.zip  -d .
+
+cd bpbx-scripts-main 
+
+./install_bpbx.sh
+
+
+systemctl disable accounts-daemon.service
+systemctl disable apparmor.service
+systemctl disable dbus-org.freedesktop.resolve1.service
+systemctl disable irqbalance.service
+systemctl disable keyboard-setup.service
+systemctl disable ondemand.service
+systemctl disable setvtrgb.service
+systemctl disable systemd-timesyncd.service
+systemctl disable ufw.service
+systemctl disable remote-fs.target
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+systemctl disable motd-news.timer
+systemctl disable ua-messaging.timer
+systemctl edit getty@tty1
+
+
+ echo ' ' > /etc/resolv.conf
+ apt-get clean
+ history -c
+ exit
+
+
+sudo mksquashfs  squashfs-root/ filesystem.squashfs -comp xz -b 1M -noappend
+
+ sudo dd if=ubuntu-20.04.4-live-server-amd64.iso bs=512 count=1 of=custom-iso/isolinux/isohdpfx.bin
+
+cp filesystem.squashfs ./iso/casper/
+ md5sum iso/.disk/info > iso/md5sum.txt
+ sed -i 's|iso/|./|g' iso/md5sum.txt
+ xorriso -as mkisofs -r -V "Ubuntu custom amd64" -o ubuntu-20.04.4-custom-amd64.iso -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot -isohybrid-gpt-basdat -isohybrid-apm-hfsplus -isohybrid-mbr /usr/lib/syslinux/bios/isohdpfx.bin iso/boot iso
+
+
+ fdisk -lu ubuntu-20.04.4-custom-amd64.iso
+ dumpet -i ubuntu-20.04.4-custom-amd64.iso  
+ xorriso -indev ubuntu-20.04.4-custom-amd64.iso -toc -pvd_info
--- install_bpbx.sh
+++ install_bpbx.sh
@@ -6,6 +6,9 @@
 #     rm "$LOG_FILE"
 # fi
 
+WORKING_DIR=$(pwd)
+
+
 echo -e "\n"
 echo -e "************************************************************"
 echo -e "*          Welcome to the BPBX  installation           *"
@@ -14,10 +17,19 @@ echo -e "*      internet connection, since the latest version of    *"
 echo -e "*                BPBX  will be installed              *"
 echo -e "************************************************************"
 
+
+echo -e "************************************************************"
+echo -e "*                   Update & Upgrade System                *"
+echo -e "************************************************************"
+apt -y update
+DEBIAN_FRONTEND=noninteractive apt -y upgrade
+apt -y clean
+
+
 echo -e "************************************************************"
 echo -e "*            Install Dependencies & SUDO                   *"
 echo -e "************************************************************"
-DEBIAN_FRONTEND=noninteractive apt install curl gpg sudo  cmake net-tools systemd-timesyncd -y
+DEBIAN_FRONTEND=noninteractive apt -y install curl gpg sudo cmake net-tools systemd-timesyncd 
 
 echo -e "************************************************************"
 echo -e "*                        Disable UFW                       *"
@@ -29,45 +41,17 @@ if [ -x "$(command -v ufw)" ]; then
 fi
 
 echo -e "************************************************************"
-echo -e "*                   Update & Upgrade System                *"
-echo -e "************************************************************"
-apt -y update
-DEBIAN_FRONTEND=noninteractive apt -y upgrade
-apt -y clean
-
-echo -e "************************************************************"
-echo -e "*           Update after setup BPBX Repo...                *"
-echo -e "************************************************************"
-apt -y update
-DEBIAN_FRONTEND=noninteractive apt -y upgrade
-apt -y clean
-
-echo -e "************************************************************"
 echo -e "*              Installing dependencies...                  *"
 echo -e "************************************************************"
-PACKAGES="ebconf-utils install redis git-core subversion wget libjansson-dev sqlite autoconf automake libxml2-dev libncurses5-dev libtool acl cron lame sox ffmpeg aptitude postfix nmap nginx fail2ban"
+PACKAGES="apt -y install install redis git-core subversion wget libjansson-dev sngrep sqlite autoconf automake libxml2-dev libncurses5-dev libtool acl cron lame sox ffmpeg aptitude postfix nmap nginx fail2ban"
 
-
-
-echo -e "************************************************************"
-echo -e "*              Web Pabackages & MariaDB...                 *"
-echo -e "************************************************************"
-PACKAGES="$PACKAGES openjdk-17-jdk openjdk-17-jre"
-PACKAGES="$PACKAGES mariadb-server unixodbc odbcinst unixodbc-dev"
 
 echo -e "************************************************************"
 echo -e "*           Installing MariaDB, JDK17       *"
 echo -e "************************************************************"
+PACKAGES="$PACKAGES openjdk-17-jdk openjdk-17-jre"
+PACKAGES="$PACKAGES mariadb-server unixodbc odbcinst unixodbc-dev"
 DEBIAN_FRONTEND=noninteractive apt install -y $PACKAGES
-
-echo -e "************************************************************"
-echo -e "*             Starting & Enabling Services...              *"
-echo -e "************************************************************"
-systemctl enable mariadb.service
-systemctl restart mariadb.service
-
-systemctl enable fail2ban
-systemctl start fail2ban
 
 echo -e "************************************************************"
 echo -e "*             Install Mariadb Driver....                   *"
@@ -79,84 +63,55 @@ cmake ../mariadb-connector-odbc/ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCONC_WITH_U
 cmake --build . --config RelWithDebInfo
 make install
 
-systemctl enable nginx
-systemctl restart nginx
+cd $WORKING_DIR
 
 echo -e "************************************************************"
-echo -e "*      Installing BPBX packages and Firewall...        *"
+echo -e "*                 Installing Asterisk packages...                 *"
 echo -e "************************************************************"
-PACKAGES="sngrep"
-PACKAGES="$PACKAGES firewalld fail2ban iptables iptables-persistent"
-DEBIAN_FRONTEND=noninteractive apt install -y $PACKAGES
 
+sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-16.30.1.tar.gz
 
+sudo tar zxf asterisk-16.30.1.tar.gz
 
-# echo -e "************************************************************"
-# echo -e "*                  Configuring Firewall...                 *"
-# echo -e "************************************************************"
+cd asterisk-*/
 
-# sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/old-releases/asterisk-16.30.1.tar.gz
+sudo contrib/scripts/get_mp3_source.sh
 
-# sudo tar zxf asterisk-16.30.1.tar.gz
+echo 'libvpb1 libvpb1/countrycode string 55' | sudo debconf-set-selections -v
 
-# cd asterisk-*/
+sudo contrib/scripts/install_prereq install
 
-# -- mp3 module
+sudo ./configure
 
-# sudo contrib/scripts/get_mp3_source.sh
-
-# echo 'libvpb1 libvpb1/countrycode string 55' | sudo debconf-set-selections -v
-
-# sudo contrib/scripts/install_prereq install
-
-# sudo ./configure
-
-# sudo make -j2
-# sudo make install
-
-
-
-
-# echo -e "************************************************************"
-# echo -e "*                  Configuring Firewall...                 *"
-# echo -e "************************************************************"
-# sed -i 's/^FirewallBackend=.*/FirewallBackend=iptables/g' /etc/firewalld/firewalld.conf
-# update-alternatives --set iptables /usr/sbin/iptables-legacy
-# update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-
-# systemctl enable firewalld.service
-# systemctl enable fail2ban.service
-
+sudo make -j2
+sudo make install
 
 # copy conf
-cp -rf ./config/asterisk/* /etc/asterisk
-cp -rf ./config/odbc/* /etc/
-cp -rf ./config/nginx/bpbx.conf /etc/nginx/conf.d/
-cp -rf ./config/fail2ban/jail.d/asterisk.conf /etc/fail2ban/jain.d/
-# get ip
+cp -rf "$WORKING_DIR/config/asterisk/*" /etc/asterisk
+cp -rf "$WORKING_DIR/config/odbc/*" /etc/
+cp -rf "$WORKING_DIR/config/nginx/bpbx.conf" /etc/nginx/conf.d/
+cp -rf "$WORKING_DIR/config/fail2ban/jail.d/asterisk.conf" /etc/fail2ban/jain.d/
 
-nginx -s reload
+# # get ip
+# read -p "Set Default UDP port (default:5060) > " udp_input
 
-read -p "Set Default UDP port (default:5060) > " udp_input
+# if [ -z "$udp_input" ]; then
+#     udp_port=5060
+# else
+#     udp_port=$udp_input
+# fi
 
-if [ -z "$udp_input" ]; then
-    udp_port=5060
-else
-    udp_port=$udp_input
-fi
+# local_ip=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
+# public_ip=$(curl -s ifconfig.me)
 
-local_ip=$(ip -o -f inet addr show | awk '/scope global/ {print $4}')
-public_ip=$(curl -s ifconfig.me)
+# if [ -n "$public_ip" ]; then
+#     echo "externaddr=${public_ip}:${udp_port}" >> /etc/asterisk/sip.conf
+# fi
 
-if [ -n "$public_ip" ]; then
-    echo "externaddr=${public_ip}:${udp_port}" >> /etc/asterisk/sip.conf
-fi
+# # update port
 
-# update port
-
-echo "udpbindaddr=0.0.0.0:${udp_port}" >> /etc/asterisk/sip.conf
-echo "localnet=${local_ip}" >> /etc/asterisk/sip.conf
-
+# echo "udpbindaddr=0.0.0.0:${udp_port}" >> /etc/asterisk/sip.conf
+# echo "localnet=${local_ip}" >> /etc/asterisk/sip.conf
 
 groupadd asterisk
 useradd -r -g asterisk -s /bin/false asterisk
@@ -166,11 +121,6 @@ echo -e "*                    Set Permissions                       *"
 echo -e "************************************************************"
 sudo chown -R asterisk: /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
 sudo chmod -R 750 /var/{lib,log,run,spool}/asterisk /usr/lib/asterisk /etc/asterisk
-
-
-sudo systemctl start asterisk 
-
-sudo systemctl enable asterisk
 
 
 echo -e "************************************************************"
@@ -186,15 +136,8 @@ sudo cp bpbx.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-
-echo -e "************************************************************"
-echo -e "*             Running the initial setup...                 *"
-echo -e "************************************************************"
-systemctl enable bpbx.service
-
 #Configure Packages
 dpkg --configure -a
-
 
 echo -e "************************************************************"
 echo -e "*             Import sql                 *"
@@ -209,11 +152,9 @@ if [ -f "db.sql" ]; then
     mysql -u "$DB_USER"  bpbx < db.sql
 fi
 
-
 echo -e "************************************************************"
 echo -e "*             copy jar and www                 *"
 echo -e "************************************************************"
-
 
 jar_dir=/opt/bpbx
 
@@ -221,8 +162,25 @@ if [ ! -d "$jar_dir" ]; then
     mkdir -p "$jar_dir"
 fi
 
-cp -rf bpbx-api.jar  /opt/bpbx/
+cp -rf "$WORKDIR/bpbx.jar"  /opt/bpbx/
 
-cp -rf web /opt/bpbx/web
+cp -rf "$WORKDIR/web" /opt/bpbx/web
 
-systemctl start bpbx.service
+
+systemctl enable mariadb.service
+systemctl restart mariadb.service
+
+systemctl enable fail2ban
+systemctl start fail2ban
+
+sudo systemctl start asterisk 
+sudo systemctl enable asterisk
+
+sudo systemctl enable nginx
+sudo systemctl start nginx
+
+sudo systemctl enable bpbx.service
+sudo systemctl start bpbx.service
+
+sudo systemctl enable fail2ban.service
+sudo systemctl start fail2ban.service
